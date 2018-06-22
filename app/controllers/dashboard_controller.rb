e15041fe05527b8de7b2ab352521739aea11be84
Gloria H <silverdragonia@gmail.com>
2018-06-22 12:08:29 -0400

    edited dashboard index

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class DashboardController < ApplicationController
   def index
     @user = current_user
+    @books = MyBook.all.order(:created_at)
+    @read_books = ReadBook.all.order(:created_at)
   end
 end
