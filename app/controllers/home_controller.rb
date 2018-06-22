e50d65aa5d6fdb4713d0eda60d5646f764fe59de
Erisay Perez <Erisaypr@gmail.com>
2018-06-22 17:40:03 -0400

    added books from db to home index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
 
   def index
+    @books = BookHistory.order("RANDOM()").limit(3)
 
     # if admin_signed_in?
     #  return redirect_to dashboard_path
