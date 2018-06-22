0094c0659e03586a1bfe4fb3fc6cfa4345270482
Gloria H <silverdragonia@gmail.com>
2018-06-22 19:54:27 -0400

    delete button link wonky - replaced with link for now

--- app/assets/javascripts/my_books.js
+++ app/assets/javascripts/my_books.js
@@ -1,2 +1,21 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$(document).ready(function() {
+  if ($('body.dashboard.index').length == 0) {
+    return;
+  }
+  $('.delete-my-book').click(function(e){
+    e.preventDefault();
+    let deletionUrl = e.target.getAttribute('href');
+    $.ajax({
+      method: 'DELETE',
+      url: deletionUrl,
+      success: function(data) {
+        $(e.target).parents('.my-book-div').remove();
+        toastr.success('Book has been successfully deleted.');
+      }
+    })
+  });
+
+});
