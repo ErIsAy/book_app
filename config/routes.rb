566d020775f2c6def2e02b9263ae82c4e3164933
Erisay Perez <Erisaypr@gmail.com>
2018-06-22 12:17:04 -0400

    fixed forms

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to:'home#index'
-    
+
   get '/search', to: 'search#index'
-  get '/search_results', to: 'search#search'    
-    
-  resources :dashboard 
+  get '/search_results', to: 'search#search'
+
+  resources :dashboard
   resources :my_books
   resources :read_books
 
